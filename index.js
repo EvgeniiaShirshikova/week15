4b22f633593fd19ed3049141535c22a8fb7618af
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-05-04 22:12:53 -0600

    1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let str = 'cold';
-
-for (let str of str) {
-  console.log(str);
-}
+const tasks = [];
+let form = document.querySelector('form');
+let task = document.getElementById('task').value;
+let buttonAdd = document.getElementById('btnadd');
+let title = document.querySelector('h2');
+let p = document.querySelector('p');
+let buttonClean = document.getElementById('btnclean');
