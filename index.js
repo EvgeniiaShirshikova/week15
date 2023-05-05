025f8cca362f539e346c1a7bafc61ab326eff1ab
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-05-05 09:56:16 -0600

    5

--- index.js
+++ index.js
@@ -12,26 +12,28 @@ let updateTaskList = () => {
   let task = taskInput.value;
   //добавляем задачу в массив
 	tasks.push(task);
-  tasks.forEach(function (task) {
-    //создаем див для элемента списка задач
-    let listElement = document.createElement('div');
-    listArea.append(listElement);
-    listElement.classList.add('listElement');
 
-    //создаем параграф с текстом задачи
-    const taskText = document.createElement('p');
-    taskText.classList.add('tasktext');
-    taskText.textContent = task;
-    listElement.append(taskText);
+  //объявляем переменную для последней в массиве задачи 
+  let lastTask = tasks.slice(-1);
 
-    //создаем чекбокс
-    const checkBox = document.createElement('input');
-    checkBox.type = "checkbox";
-    listElement.append(checkBox);
-  })
+  //создаем див для элемента списка задач
+  let listElement = document.createElement('div');
+  listArea.append(listElement);
+  listElement.classList.add('listElement');
 
-  //стираем тест нет задач
-  textNoTasks.textContent = '';
+  //создаем параграф с текстом задачи
+  const taskText = document.createElement('p');
+  taskText.classList.add('tasktext');
+  taskText.textContent = lastTask;
+  listElement.append(taskText);
+
+  //создаем чекбокс
+  const checkBox = document.createElement('input');
+  checkBox.type = "checkbox";
+  listElement.append(checkBox);
+
+  //удалаем параграф нет задач
+  textNoTasks.remove();
 
   //очищаем форму
   form.reset();
